f0400850a544aed07076eb7ba7822159da544566
lucastlx <102834666+lucastlx@users.noreply.github.com>
2022-11-21 23:54:39 +0000

    21/12 p1

--- project/src/App.java
+++ project/src/App.java
@@ -6,10 +6,17 @@ import Biblioteca.Livro;
 import Biblioteca.Usuario;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Endereco endereco = new Endereco("****", "Junco", "Picos", "PI", 260);
-        Usuario usuario = new Usuario("Ézio", "teste@teste.com", LocalDate.of(2001,9,10) , endereco);
-        Livro livro = new Livro("***", "***", "***", LocalDate.of(0000,0,0), 1);
-        Emprestimo emprestimo = new Emprestimo(usuario, LocalDate.of(2022, 11, 13), null);
-    }
+  public static void main(String[] args) throws Exception {
+    Endereco endereco = new Endereco("****", "Junco", "Picos", "PI", 260);
+
+    Usuario usuario = new Usuario("Ézio", "teste@teste.com", LocalDate.of(2001, 9, 10), endereco);
+
+    Livro livro = new Livro("***", "***", "***", LocalDate.of(0000, 0, 0), 1);
+
+    Emprestimo emprestimo = new Emprestimo(usuario, LocalDate.of(2022, 11, 13), null);
+
+    System.out.println(
+        "Digite:\n [1] Configuração de livro \n [2] Configuração de usuário \n [3] Empréstimo de livro \n [4] Sair do app ");
+  }
+
 }
