5cef63726443b4942c47d15bb170c86d0bc5c02b
Ruan Lauro <ruanlauromonteiro@gmail.com>
2022-11-21 21:13:15 -0300

    21/11

--- project/src/App.java
+++ project/src/App.java
@@ -1,22 +1,32 @@
 import java.time.LocalDate;
-
+import java.util.Scanner;
 import Biblioteca.Emprestimo;
 import Biblioteca.Endereco;
 import Biblioteca.Livro;
 import Biblioteca.Usuario;
 
-public class App {
+public class App {  
   public static void main(String[] args) throws Exception {
     Endereco endereco = new Endereco("****", "Junco", "Picos", "PI", 260);
-
+    
     Usuario usuario = new Usuario("Ézio", "teste@teste.com", LocalDate.of(2001, 9, 10), endereco);
-
-    Livro livro = new Livro("***", "***", "***", LocalDate.of(0000, 0, 0), 1);
-
+    
+    Livro livro = new Livro("***", "***", "***", LocalDate.of(2000, 1, 10), 1);
+    
     Emprestimo emprestimo = new Emprestimo(usuario, LocalDate.of(2022, 11, 13), null);
+    
+    Scanner leitor = new Scanner(System.in);
+
+    int opcao = 0;
 
     System.out.println(
         "Digite:\n [1] Configuração de livro \n [2] Configuração de usuário \n [3] Empréstimo de livro \n [4] Sair do app ");
+    opcao = leitor.nextInt();
+  
+    switch(opcao) {
+      case 1:
+        System.out.println("Digite: [1] Cadastro | [2]"); 
+    }
   }
 
 }
